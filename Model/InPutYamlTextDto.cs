13aed342b3facacc200e4e86188adac8b4448b31
wwxawwx <52812016+wq969453423@users.noreply.github.com>
2022-07-10 20:40:17 +0800

    Model

---
+++ Model/InPutYamlTextDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 子端.Model
+{
+    public class InPutYamlTextDto
+    {
+       
+        public List<string> path { get; set; }
+
+        public string minerName { get; set; }
+
+        public string apiKey { get; set; }
+
+        public string cpuIndex { get; set; }
+        public string proxy { get; set; }
+
+        // argon2avx2,largepages,hardaes,fullmem,jit
+        public string flag { get; set; }
+
+        public string threadNum { get; set; }
+    }
+}
